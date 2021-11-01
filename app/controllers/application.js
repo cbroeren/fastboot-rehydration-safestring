614ac27358e3dc5c2d10795fb7ce5459f2b46f04
Chantal Broeren <chantal.broeren@dockyard.com>
2021-11-01 22:09:39 +0100

    Example for Fastboot rehydration with SafeString

---
+++ app/controllers/application.js
@@ -0,0 +1,32 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import { guidFor } from '@ember/object/internals';
+import { htmlSafe } from '@ember/template';
+
+export default class ApplicationController extends Controller {
+    @service fastboot;
+    @tracked value;
+
+    get safeValue() {
+        const safe = htmlSafe(this.value);
+        return safe;
+    }
+
+    @action
+    generateNewValue() {
+        this.value = `<span>Random ${guidFor({})}</span>`;
+    }
+
+    constructor() {
+        super(...arguments);
+
+        if (this.fastboot.isFastBoot) {
+            this.value = "<span>Fastboot Initial Value</span>";
+        } else {
+            this.generateNewValue();
+        }
+
+    }
+}
